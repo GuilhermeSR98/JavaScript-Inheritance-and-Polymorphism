d39f963a02f5f0a630c67f02578d18b0df2571b8
GuilhermeSR98 <guigareisbr@gmail.com>
2021-07-03 03:05:14 -0300

    Duck Type

--- AuthenticationSystem.js
+++ AuthenticationSystem.js
@@ -1,3 +1,4 @@
+//Duck typing in computer programming is an application of the duck test—"If it walks like a duck and it quacks like a duck, then it must be a duck"—to determine whether an object can be used for a particular purpose. With normal typing, suitability is determined by an object's type.
 export class AuthenticationSystem{
     static isAuthenticateable(authenticateable){
         return 'authenticate' in authenticateable &&
